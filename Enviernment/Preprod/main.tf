b71890ed0e3c99f8e9a3ed69b771f8db8f758eca
Abhisek-IT <abhisek.yadav.it.148@gmail.com>
2026-08-12 01:15:20 +0530

    Update main.tf

--- Enviernment/Preprod/main.tf
+++ Enviernment/Preprod/main.tf
@@ -4,6 +4,7 @@ module "resource_group" {
 }
 
 module "storage_account" {
+depends_on = [module.resource_group]
     source = "../../Module/storage account/"
   storage_account_m =var.storage_account_p
 }
