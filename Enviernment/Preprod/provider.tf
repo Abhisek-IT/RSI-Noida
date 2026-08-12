5ec0248fe649e06b9c28fa00261931f92b35c003
Abhisek-IT <abhisek.yadav.it.148@gmail.com>
2026-08-13 02:56:13 +0530

    Update provider.tf

--- Enviernment/Preprod/provider.tf
+++ Enviernment/Preprod/provider.tf
@@ -1,16 +1,9 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "5.0.1"
-    }
-  }
-}
-
 provider "azurerm" {
-  features { }
-client_id= "748dd8b6-a1ac-4619-a23e-8717bb00c4f0"
-          tenant_id= "4eda09ca-f09f-4db9-86ae-c6b2e0ffc6ce"
-          subscription_id= "0b3701d7-b87c-4c64-ba31-5ff3f5843d15"
- use_oidc = true
+  features {}
+
+  subscription_id = "0b3701d7-b87c-4c64-ba31-5ff3f5843d15"
+  tenant_id       = "4eda09ca-f09f-4db9-86ae-c6b2e0ffc6ce"
+  client_id       = "748dd8b6-a1ac-4619-a23e-8717bb00c4f0"
+
+  use_oidc = true
 }
