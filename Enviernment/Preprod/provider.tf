db7551defeda5551e0c9607d997fa2de17e6bd35
Abhisek-IT <abhisek.yadav.it.148@gmail.com>
2026-08-13 02:40:01 +0530

    Update provider.tf

--- Enviernment/Preprod/provider.tf
+++ Enviernment/Preprod/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features { }
-client_id= "a6de05a4-b6be-4f05-a4eb-b7f9748eb800"
+client_id= "748dd8b6-a1ac-4619-a23e-8717bb00c4f0"
           tenant_id= "4eda09ca-f09f-4db9-86ae-c6b2e0ffc6ce"
           subscription_id= "0b3701d7-b87c-4c64-ba31-5ff3f5843d15"
  use_oidc = true
