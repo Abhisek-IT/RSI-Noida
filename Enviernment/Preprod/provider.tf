fe75b4491c5c4d98e5c40bd010c651f59c0eeff1
Abhisek-IT <abhisek.yadav.it.148@gmail.com>
2026-08-13 13:34:50 +0530

    Update provider.tf

--- Enviernment/Preprod/provider.tf
+++ Enviernment/Preprod/provider.tf
@@ -1,9 +1,4 @@
 provider "azurerm" {
   features {}
-
-AZURE_CLIENT_ID = "748dd8b6-a1ac-4619-a23e-8717bb00c4f0"
-AZURE_TENANT_ID = "4eda09ca-f09f-4db9-86ae-c6b2e0ffc6ce"
-AZURE_SUBSCRIPTION_ID = "0b3701d7-b87c-4c64-ba31-5ff3f5843d15"
-
   use_oidc = true
 }
